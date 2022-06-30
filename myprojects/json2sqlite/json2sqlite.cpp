e8e4b1fcff529a5d37c7f3cf9241755e03584b0e
wangzf <wangzf@mchz.com.cn>
2022-06-30 14:33:12 +0800

    add json2sqlite

---
+++ myprojects/json2sqlite/json2sqlite.cpp
@@ -0,0 +1,14 @@
+// json2sqlite.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "main_application.h"
+
+int main(int argc, char* argv[])
+{
+	json2sqlite::MainApplication app;
+	app.Run(argc, argv);
+
+    return 0;
+}
+
---
+++ myprojects/json2sqlite/main_application.cpp
@@ -0,0 +1,21 @@
+#include "main_application.h"
+
+#include ""
+
+namespace json2sqlite {
+	int MainApplication::Run(int argc, char* argv[]) {
+
+	}
+
+	bool MainApplication::HandleCommandLine(int argc, char* argv[]) {
+
+	}
+
+	MainApplication::MainApplication() {
+
+	}
+
+	MainApplication::~MainApplication() {
+
+	}
+}
